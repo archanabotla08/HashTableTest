829a45c1e597e5cfc573d7200cf0d4654ba3ac30
Nisha271 <bukkenisha@gmail.com>
2020-11-06 16:54:58 +0530

    [Archana] Added: The welcome message in source/main

---
+++ src/main/java/com/blz/hashtableimplementationtest/HashMap.java
@@ -0,0 +1,8 @@
+package com.blz.hashtableimplementationtest;
+
+public class HashMap<K,V> {
+	public static void main(String[] args) {
+		System.out.println("Welcome Message");
+	}
+
+}
---
+++ src/test/java/com/blz/hashtableimplementationtest/HashTableTest.java
@@ -0,0 +1,16 @@
+package com.blz.hashtableimplementationtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class HashTableTest {
+
+	@Test
+	public void test() {
+		fail("implementation");
+		
+	}
+
+}
